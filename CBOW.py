5c02e21327f8d0e241705e119e6abe88764939fc
QiYixi <qiyixi2010@gmail.com>
2021-10-26 17:23:39 +0900

    renew the model

--- CBOW.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import word2vec
-file = open('./cor.txt','r','sjis')
-word_list = file.read()
-my_list = word_list.split(',')
-print(my_list[0])
--- main.py
+++ main.py
@@ -4,52 +4,51 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import re
-# from janome.tokenizer import Tokenizer
-# import CBOW
-# file = open('/Users/qiyixi/Downloads/Pycharm/corpus20150426.txt','r',encoding='sjis')
-# text = file.read()
-# #print(text[100])
-# t = Tokenizer()
+from janome.tokenizer import Tokenizer
+import CBOW
+file = open('/Users/qiyixi/Downloads/Pycharm/corpus20150426.txt','r',encoding='sjis')
+text = file.read()
+#print(text[100])
+t = Tokenizer()
 #
-# def extract_words(test):
-#     tokens = t.tokenize(test)
-#     return [token.base_form for token in tokens
-#         if token.part_of_speech.split(',')[0] in['名詞','動詞','形容詞']]
-#
-#
+def extract_words(test):
+    tokens = t.tokenize(test)
+    return [token.base_form for token in tokens
+        if token.part_of_speech.split(',')[0] in['名詞','動詞','形容詞']]
 #
 #
-# sentences = text.split('。')
+#
+#
+sentences = text.split('。')
 # #tokens = t.tokenize(sentences[2])
 # #print(tokens)
 # # for token in tokens:
 # #     print(token)
 # #word_list = [extract_words(sentences[2])]
 #
-# word_list = [extract_words(sentence)for sentence in sentences]
+word_list = [extract_words(sentence)for sentence in sentences]
 #
-# txt = open('./cor.txt','w',encoding='sjis')
+# txt = open('./corpus.txt','w',encoding='sjis')
 # for word_2 in word_list:
 #     for word_1 in word_2:
 #         txt.write(word_1)
 #         txt.write(',')
-#     txt.write('\r\n')
 # txt.close()
 # print(word_list)
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
 
 from gensim.models import word2vec
-file = open('./cor.txt','r',encoding='sjis')
-word_list = file.read()
-my_list = word_list.split(',')
-#print(my_list[0])
-model = word2vec.Word2Vec(my_list,sg=1)
-ret = model.wv.most_similar(positive=['南'])
+# file = open('./corpus.txt','r',encoding='sjis')
+# word_list = file.read()
+# my_list = word_list.split(',')
+# print(my_list[:100])
+model = word2vec.Word2Vec(word_list,min_count=2,sg=0)
+ret = model.wv.most_similar(positive=['佐川'])
 for item in ret :
     print(item[0],item[1])
 model.save('./Word_CBOW.w2v')
+# file.close()
 # Press the green button in the gutter to run the script.
 #if __name__ == '__main__':
     #print_hi('PyCharm')
